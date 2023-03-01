a3f7cc2cbcb9ccddc518a42e72fe751cabd53b41
Bilal <79476137+mbilaldemirci@users.noreply.github.com>
2023-03-01 17:38:30 +0300

    ewew
    wewewewewewew

---
+++ Win11 Pro Activator by Simyaci.cmd
@@ -0,0 +1,75 @@
+:: Section 1: Windows 11 Pro Aktive Etme
+
+
+@echo off
+
+color 4
+
+ECHO ==========================
+ECHO Simyaci Win 11 Pro Aktivasyon Crack
+ECHO ==========================
+
+color 4
+ECHO                  __    _                                   
+ECHO                     _wr""        "-q__                             
+ECHO                  _dP                 9m_     
+ECHO                _#P                     9#_                         
+ECHO               d#@                       9#m                        
+ECHO              d##                         ###                       
+ECHO             J###                         ###L                      
+ECHO              ###K                       J###K                      
+ECHO              ####K      ___aaa___      J####F                      
+ECHO         __gmM######_  w#P""   ""9#m  _d#####Mmw__                  
+ECHO      _g##############mZ_         __g##############m_               
+ECHO    _d####M@PPPP@@M#######Mmp gm#########@@PPP9@M####m_             
+ECHO   a###""          ,Z"#####@" '######"\g          ""M##m            
+ECHO  J#@"             0L  "*##     ##@"  J#              *#K           
+ECHO  #"               `#    "_gmwgm_~    dF               `#_          
+ECHO 7F                 "#_   ]#####F   _dK                 JE          
+ECHO                      *m__ ##### __g@"                   F          
+ECHO                        "PJ#####LP"                                 
+ECHO  `                       0######_                      '           
+ECHO                        _0########_                                   
+ECHO      .               _d#####^#####m__              ,              
+ECHO       "*w_________am#####P"   ~9#####mw_________w*"                  
+ECHO           ""9@#####@M""           ""P@#####@M""                    
+
+
+
+ECHO ============================
+ECHO Onemli Not: 
+ECHO Az sonra kurulum islemi sirasinda bazi pencereler acilacak bu pencereleri onaylayarak kapatmaniz gerekmekte!
+ECHO 10 Saniye icerisinde islemler baslayacak!
+ECHO ============================
+
+
+TIMEOUT /T 10
+
+PAUSE
+
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+cls
+
+slmgr /skms kms8.msguides.com
+cls
+
+slmgr /ato
+cls
+
+color 3
+cls
+
+ECHO ============================
+
+ECHO Windows 11 Pro Sorunsuz Bir Sekilde Aktive Edildi!
+ECHO Instagram: @mbilaldemirci
+
+ECHO ============================
+
+
+TIMEOUT /T 4
+
+
+START https://instagram.com/mbilaldemirci
+
+PAUSE
